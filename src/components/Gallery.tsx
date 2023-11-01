b1e203eca6fe27f57924bc5890ee9e94cc4e74cb
Sumon Chandra <topu199041@gmail.com>
2023-11-01 12:12:03 +0600

    Modify header and delete images

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -3,7 +3,7 @@ import { images as initialImages } from "../primitives/images"
 import Image from "./Image"
 
 const Gallery = () => {
-    const [images, setImages] = useState(initialImages)
+    const [images, setImages] = useState(initialImages.sort((a, b) => a.id - b.id))
     const [selectedImages, setSelectedImages] = useState<number[]>([])
 
     const toggledImage = (id: number) => {
@@ -18,18 +18,32 @@ const Gallery = () => {
         setSelectedImages([])
     }
 
+    const headerContent = () => {
+        if (selectedImages.length === 0) {
+            return (
+                <div className="py-2 border-b-4">
+                    <h3 className="text-xl font-bold">Gallery</h3>
+                </div>
+            )
+        } else {
+            return (
+                <div className="flex items-center justify-between py-2 border-b-4">
+                    <p className="text-lg font-semibold"><span className="text-xl font-black">{selectedImages?.length}</span> File Selected</p>
+                    <button
+                        onClick={deleteSelectedImages}
+                        className="px-2 py-1 text-xs font-semibold text-white bg-red-500 rounded-md"
+                    >
+                        Delete images
+                    </button>
+                </div>
+            )
+        }
+    }
+
     return (
         <div className="p-5 m-5 bg-white border rounded-lg shadow lg:m-0">
-            <div className="flex items-center justify-between py-4">
-                <p className="text-lg font-semibold">Selected Images: {selectedImages?.length}</p>
-                <button
-                    onClick={deleteSelectedImages}
-                    className="px-2 py-1 text-xs font-semibold text-white bg-red-500 rounded-md"
-                >
-                    Delete images
-                </button>
-            </div>
-            <div className="grid grid-flow-col grid-rows-3 gap-6 mx-auto lg:max-w-3xl">
+            {headerContent()}
+            <div className="grid grid-flow-row grid-cols-5 grid-rows-3 gap-6 mx-auto mt-4 lg:max-w-3xl">
                 {
                     images.map((image, index) => (
                         <Image
--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -5,35 +5,41 @@ interface ImageProps {
     onToggle: (id: number) => void;
     selectedImages: number[]
 }
+
 const Image: FC<ImageProps> = ({ image, order, onToggle, selectedImages }) => {
-    return (
-        <div className={`border-2 relative flex items-center justify-center rounded-md ${order === 1 ? "row-span-2 col-span-2" : image.img === "" ? " border-4 border-gray-400 border-dotted" : "row-span-1"}`}>
-            {
-                image.img ? (
-                    <div className="cursor-pointer">
-                        <img
-                            src={image.img}
-                            alt="Product Image"
-                            className="h-full rounded-md"
-                        />
-                        <div className={`absolute z-10 top-0 left-0 w-full h-full transition-opacity bg-[#00000071] opacity-0 rounded-md
+
+    const content = () => {
+        if (image.img) {
+            return (
+                <div className="cursor-pointer">
+                    <img
+                        src={image.img}
+                        alt="Product Image"
+                        className="h-full rounded-md"
+                    />
+                    <div className={`absolute z-10 top-0 left-0 w-full h-full transition-opacity bg-[#00000071] opacity-0 rounded-md
                         ${selectedImages.includes(image.id) ? "opacity-100" : "opacity-0"} 
                         hover:opacity-100`}
-                        >
-                            <input
-                                type="checkbox"
-                                onChange={() => onToggle(image.id)}
-                                checked={selectedImages.includes(image.id)}
-                                className="absolute z-50 w-5 h-5 opacity-100 cursor-pointer top-2 left-2"
-                            />
-                        </div>
+                    >
+                        <input
+                            type="checkbox"
+                            onChange={() => onToggle(image.id)}
+                            checked={selectedImages.includes(image.id)}
+                            className="absolute z-50 w-5 h-5 opacity-100 cursor-pointer top-2 left-2"
+                        />
                     </div>
-                ) : (
-                    <div className="absolute left-0 w-full text-center top-14">
-                        <span className="text-xs font-bold ">Add image</span>
-                    </div>
-                )
-            }
+                </div>
+            )
+        } else {
+            return <div className="absolute left-0 w-full text-center top-14">
+                <span className="text-xs font-semibold">Add image</span>
+            </div>
+        }
+    }
+
+    return (
+        <div className={`border-2 relative flex items-center justify-center rounded-md ${order === 1 ? "row-span-2 col-span-2" : image.img === "" ? " border-4 border-gray-400 border-dotted" : "row-span-1"}`}>
+            {content()}
         </div>
     )
 }
